c03e91f6f900b0fc746744634d268619335afd86
DeaKenas <73515265+DeaKenas@users.noreply.github.com>
2021-05-11 21:26:06 -0700

    Update P1nger.py

--- P1nger.py
+++ P1nger.py
@@ -14,27 +14,27 @@ print("                         $$\   $$ |                  ")
 print("                         \$$$$$$  |                  ")
 print("                          \______/                   ")
 
+print("Thanks for using my custom pinger!")
+
 def ipping():
-	os.system("cls")
-	count = 1
-	e = input("Enter IP Address:   ")
-	replies = 0
-	replies += 1
-	hostname = e
-	os.system("cls")
-	print("-" * 100)
-	print("=" * 100)
-	print("-" * 100)
-	while True:
-		response = os.system("ping -n 1 " + hostname + " >nul")
-		if response == 0:
-			print("\033[1;32;40m" + hostname + " is getting p1nged!" + " [" +
-			      str(count) + "]" + '\033[0m')
-		else:
-			print("[-] " + hostname + " is p1nged off"
-			      " [" + str(count) + "]" + '\033[0m')
-		count += 1
-		time.sleep(0)
+    os.system("cls")
+    count = 1
+    e = input("Enter IP Address:   ")
+    replies = 0
+    replies += 1
+    hostname = e
+    os.system("cls")
+    print("-"*100)
+    print("="*100)
+    print("-"*100)
+    while True:
+        response = os.system("ping -n 1 " + hostname + " >nul")
+        if response == 0:
+            print("\033[1;32;40m" + hostname + " is being P1NGED" + " [" +  str(count) + "]" +  '\033[0m')
+        else:
+            print("\033[31m" + hostname + " offline" " [" +  str(count) + "]" +  '\033[0m')
+        count += 1
+        time.sleep(1)
 
 
 ipping()
